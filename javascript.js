c40542f036e5470340339bea11d01df02e73d403
Anson <ansonwong142@gmail.com>
2025-01-03 22:13:29 -0500

    create function to play a single round

--- javascript.js
+++ javascript.js
@@ -12,10 +12,31 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt('What is your choice?')
+    let humanChoice = prompt('What is your choice?').toLowerCase();
     return humanChoice;
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-console.log('human choice: '+humanChoice);
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        humanScore++;
+        computerScore++;
+        console.log('human and computer both win this round');
+    }
+    else if(humanChoice === 'rock' && computerChoice === 'scissor' 
+        || humanChoice ==='paper' && computerChoice ==='rock'
+        || humanChoice ==='scissor' && computerChoice ==='paper'){
+            humanScore++;
+            console.log('human wins this round');
+    }
+    else{
+        computerScore++;
+        console.log('computer wins this round');
+    }
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(humanChoice, computerChoice);
